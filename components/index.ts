c199d29ce10064b5e186a654d395e0087f6db3dc
Gyula Szvitek <gy.szvitek@gmail.com>
2023-09-12 14:45:05 +0100

    feat(layout): navbar and footer added, hero fix

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,6 @@
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
 import Hero from "./Hero";
+import Navbar from "./Navbar";
 
-export {
-  Hero,
-  CustomButton
-};
+export { CustomButton, Footer, Hero, Navbar };
