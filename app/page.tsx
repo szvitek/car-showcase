4fef79a4e99e8706b2233be835a44e299756e011
Gyula Szvitek <gy.szvitek@gmail.com>
2023-09-19 13:03:33 +0100

    feat(SearchBar): dynamic search implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { FilterProps } from "@/types";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: FilterProps;
+}) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2023,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
