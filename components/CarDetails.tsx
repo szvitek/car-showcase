2b8f48a47fa320e5bef6d2d29bc6bab99d6c866d
Gyula Szvitek <gy.szvitek@gmail.com>
2023-09-19 11:48:28 +0100

    feat(CarImages): car images api integrated

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
 import { Fragment } from "react";
@@ -12,6 +13,7 @@ interface CarDetailsProps {
 }
 
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
+  const angles = ["29", "33", "13"];
   return (
     <>
       <Transition appear show={isOpen} as={Fragment}>
@@ -57,7 +59,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill
                         priority
@@ -66,33 +68,20 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     </div>
 
                     <div className="flex gap-3">
-                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                        <Image
-                          src="/hero.png"
-                          alt="car model"
-                          fill
-                          priority
-                          className="object-contain"
-                        />
-                      </div>
-                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                        <Image
-                          src="/hero.png"
-                          alt="car model"
-                          fill
-                          priority
-                          className="object-contain"
-                        />
-                      </div>
-                      <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                        <Image
-                          src="/hero.png"
-                          alt="car model"
-                          fill
-                          priority
-                          className="object-contain"
-                        />
-                      </div>
+                      {angles.map((angle) => (
+                        <div
+                          className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg"
+                          key={angle}
+                        >
+                          <Image
+                            src={generateCarImageUrl(car, angle)}
+                            alt="car model"
+                            fill
+                            priority
+                            className="object-contain"
+                          />
+                        </div>
+                      ))}
                     </div>
                   </div>
 
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
   const options = {
@@ -26,4 +28,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const apiKey = process.env.NEXT_PUBLIC_IMAGE_API_KEY;
+  const { make, model, year } = car;
+
+  const url = new URL("https://cdn.imagin.studio/getImage");
+  url.searchParams.append("customer", apiKey as string);
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return url.toString();
 };
