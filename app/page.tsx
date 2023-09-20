d1fcfabd8f0bbe64fe3ab402854438835587efee
Gyula Szvitek <gy.szvitek@gmail.com>
2023-09-20 14:28:23 +0100

    feat(CustomFilter): custom filter implemented with update url search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
 import { FilterProps } from "@/types";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
@@ -32,8 +33,8 @@ export default async function Home({
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
